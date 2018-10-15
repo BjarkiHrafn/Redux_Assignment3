6566ff8d0678a83600316b3aacd94e94643abb43
Leifur16 <leifur16@ru.is>
2018-10-15 15:05:51 +0000

    here u go u bits

--- Reducers/todoReducer.js
+++ Reducers/todoReducer.js
@@ -11,28 +11,12 @@ const initialState = {
 const todoReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TODO:
-    console.log("added: ", action.payload);
+      console.log("added: ", action.payload);
       return {
         ...state,
         todos: state.todos.concat(action.payload)
       };
-<<<<<<< HEAD
-      case DELETE_TODO:
-        return {
-          ...state,
-          todos: state.todos.filter(item => {
-            console.log(item.task, " !=== ", action.payload, ": ", item.task !== action.payload)
-            item.task !== action.payload.task})
-        }
-        case UPDATE_DONE_TODO:
-          return {
-            ...state,
-            todos: state.todos.map(todo => {
-              if(todo.task === action.payload.task) {
-                todo.done = !todo.done;
-              }
-            })
-=======
+
     case DELETE_TODO:
       return {
         ...state,
@@ -44,7 +28,6 @@ const todoReducer = (state = initialState, action) => {
         todos: state.todos.map(todo => {
           if (todo.task === action.payload.task) {
             todo.done = !todo.done;
->>>>>>> f46f9adefe1de447c0f7773174159c3f7354360a
           }
           return todo;
         })
